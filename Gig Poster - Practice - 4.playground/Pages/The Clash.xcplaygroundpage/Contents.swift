7619eef217c45d140a181abb5e37632171094dce
Justus Post <JPost@lcs.on.ca>
2020-02-27 20:37:56 -0500

    finished it, did the structures, the background and the text

--- Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
@@ -34,6 +34,54 @@ let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
 // Begin your work below...
 
 
+// change the background color
+
+canvas.fillColor = orange
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+
+// write the things
+canvas.drawText(message: "the clash", at: Point(x: 20, y: 94), size: 46)
+
+canvas.drawText(message: "with the brattles\nand dear kennedys", at: Point(x: 20, y: 30), size: 8)
+
+canvas.drawText(message: "matinee and eve\nsatruday/june 13 1981", at: Point(x: 106, y: 30), size: 8)
+
+canvas.drawText(message: "bonds international casino\nin times square, new york city", at: Point(x: 258, y: 30), size: 8)
+// translate the canvas
+canvas.translate(to: Point(x: 230, y: 376))
+
+
+// rotate the canvas
+canvas.rotate(by: 45)
+
+
+// create the rectangle pattern using a loop
+
+canvas.defaultBorderWidth = 10
+canvas.borderColor = offWhite
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+var measurements = 10
+for _ in 1...7 {
+    canvas.drawRectangle(at: Point(x: 0, y: 0), width: measurements, height: measurements)
+    canvas.translate(to: Point(x: -24, y: -24))
+    measurements += 48
+}
+
+canvas.borderColor = blue
+canvas.rotate(by: -45)
+canvas.translate(to: Point(x: -60, y: 238))
+var measurements2 = 10
+canvas.rotate(by: 45)
+for _ in 1...7 {
+    canvas.drawRectangle(at: Point(x: 0, y: 0), width: measurements2, height: measurements2)
+    canvas.translate(to: Point(x: -24, y: -24))
+    measurements2 += 48
+}
+
+
+
 /*:
  ## Use Source Control
  
